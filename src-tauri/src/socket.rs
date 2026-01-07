0c578a73bc6a15bfc2803ace0ed2af4149bdf695
LandonSiler <paypal.silerlandon@gmail.com>
2026-01-06 20:24:32 -0500

    feat: Add tauri and tooling

---
+++ src-tauri/src/socket.rs
@@ -0,0 +1,8 @@
+// use tokio::io::{AsyncReadExt, AsyncWriteExt};
+// use tokio::net::{TcpListener, TcpStream};
+
+// #[tokio::main]
+// async fn listen_at(address: &str) -> tokio::io::Result<tokio::net::TcpListener> {
+//     let listener = TcpListener::bind(address).await?;
+//     Ok(listener)
+// }
